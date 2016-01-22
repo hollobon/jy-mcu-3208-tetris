72c45a584acf2276c3dd6716e37f9e25b2ec7e12
Pete Hollobon <pete@hollobon.com>
2016-01-22 21:15:21 +0000

    Make message queue mutations atomic

--- mq.c
+++ mq.c
@@ -6,6 +6,7 @@
 #include "mq.h"
 #include <stdbool.h>
 #include <stdint.h>
+#include <util/atomic.h>
 
 volatile message_t _mq[MQ_SIZE];
 volatile uint8_t _mq_front = 0;
@@ -16,8 +17,11 @@ bool mq_put(message_t value)
     if (mq_space < 1)
         return false;
 
-    _mq[_mq_front] = value;
-    _mq_front = (_mq_front + 1) % MQ_SIZE;
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+        _mq[_mq_front] = value;
+        _mq_front = (_mq_front + 1) % MQ_SIZE;
+    }
     return true;
 }
 
@@ -26,7 +30,10 @@ bool mq_get(message_t *value)
     if (mq_empty)
         return false;
 
-    *value = _mq[_mq_back];
-    _mq_back = (_mq_back + 1) % MQ_SIZE;
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
+    {
+        *value = _mq[_mq_back];
+        _mq_back = (_mq_back + 1) % MQ_SIZE;
+    }
     return true;
 }
